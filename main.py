4da3f09788996ed48b22ead621ef8b7dd1cd90d6
Delmag1 <cheek_robert@hotmail.com>
2025-05-14 23:20:19 +0200

    player added and movable

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import pygame
 from constants import *
+from player import *
+
 print("Starting Asteroids!")
 print(SCREEN_WIDTH)
 print(SCREEN_HEIGHT)
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-def game_loop():
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
-    
-    while True:
+    while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
-
-
-
+        screen.fill("black")
+        dt = clock.tick(60) / 1000  # Get delta time in seconds
+        player.move(dt)
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()  # Update the display
 
 if __name__ == "__main__":
-    game_loop()
+    main()
